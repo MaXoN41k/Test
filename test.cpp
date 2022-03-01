a9e4df1a51a32100de2e40baf3f31a23b99c96e0
MaXoN41k <zabot1n.m@yandex.ru>
2022-03-01 23:01:24 +0300

    change name

--- test.cpp
+++ test.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	TInstitut institut("IITMM");
 	cout << institut.GetInstitut() << endl;
-	THuman human("Zabotin Maxim",2003,38210150);
+	THuman human("Mosyagin Maxim",2003,38210150);
 	cout << human.GetName() << endl;
-	TStudent student("Zabotin Maxim", 2003, 38210150, 2021);
+	TStudent student("Mosyagin Maxim", 2003, 38210150, 2022);
 	return 0;
 }
